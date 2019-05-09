e4962e768421f531ea0f766de98179ae564e9dcb
Sasirekha Muthiah <sasirekha.muthiah@hivehome.com>
2019-05-09 13:32:36 +0100

    Add files via upload

---
+++ multiconf.js
@@ -0,0 +1,12 @@
+exports.config={
+    framework:'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs:['spec.js'],
+    multiCababilities: [{
+        browserName: 'firfox'
+    },
+    {
+        browserName: 'chrome'
+    }
+    ]
+};  
